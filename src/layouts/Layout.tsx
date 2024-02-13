fc89e7d102c8f346d0bd8bb36a28b2d6915d8756
Rafael Rodriguez <rafaelrodriguez111294@gmail.com>
2024-02-13 10:22:08 -0400

    fix

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { ContactForm } from '@/components/ContactForm';
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: '/portfolio/',
     plugins: [react()],
     css: {
       postcss: {
